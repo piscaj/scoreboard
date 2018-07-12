e5732649e7955c0335e73648a3b6069ee171e02d
Jason Piscatelli <JasonPiscatelli@Jason-Sr.local>
2018-07-12 15:50:26 -0400

    Added protocol testing stuff

--- src/protocolTest.js
+++ src/protocolTest.js
@@ -1,22 +1,25 @@
 
 import React, { Component } from 'react';
+import { toHex } from './convertHex.js';
+
 
 class DigitProtocolTest extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        data: 'Enter Score...'
+        text: 'Enter Score...'
       }
       this.updateState = this.updateState.bind(this);
     };
     updateState(e) {
-      this.setState({data: e.target.value});
+      //Change text string to ascii character and convert to hex.
+      this.setState({text: e.target.value.charCodeAt(0).toString(16)}); 
     }
     render() {
       return(
         <div>
-        <input type="text" value = {this.state.data} onChange = {this.updateState}/>
-          <h4>{this.state.data}</h4>
+        <input type="text" onChange = {this.updateState}/>
+          <h4>{this.state.text}</h4>
         </div>
       );
     }
