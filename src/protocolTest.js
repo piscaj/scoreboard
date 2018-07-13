ce25529839cee5674a05c2c61b93ba223dc87436
Jason Piscatelli <JasonPiscatelli@dhcp-wifi-8021x-155-41-11-150.bu.edu>
2018-07-13 12:12:12 -0400

    Building protocol functions

--- src/protocolTest.js
+++ src/protocolTest.js
@@ -1,25 +1,31 @@
 
 import React, { Component } from 'react';
 import { toHex } from './convertHex.js';
+import { calculateChk } from './calculateChecksum..js';
 
 
 class DigitProtocolTest extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        text: 'Enter Score...'
+        myStringIsNowHex: '00',
+        buildDisplayCommand: '00'
       }
       this.updateState = this.updateState.bind(this);
     };
     updateState(e) {
       //Change text string to ascii character and convert to hex.
-      this.setState({text: e.target.value.charCodeAt(0).toString(16)}); 
+      this.setState({myStringIsNowHex: e.target.value.charCodeAt(0).toString(16)});
+      this.setState({buildDisplayCommand: e.target.value.charCodeAt(0).toString(16)}); 
     }
     render() {
       return(
         <div>
         <input type="text" onChange = {this.updateState}/>
-          <h4>{this.state.text}</h4>
+          <h4>User input to hex: 0x{this.state.buildDisplayCommand === 'NaN' ? '00' : this.state.buildDisplayCommand}</h4>
+          <p>
+          <h4>01 44 30 31 {this.state.buildDisplayCommand === 'NaN' ? '00' : this.state.buildDisplayCommand} Chk Chk 03</h4>
+          </p>
         </div>
       );
     }
