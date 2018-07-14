531f6d99a3c37a4f1b6c78c219c96cd4941f6aa4
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-07-14 17:26:02 -0400

    Protocol testing

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const  SOH = 0x01
+export const  DIGIT = 0x44
+export const  ID0 = 0x30 
+export const  ID1 = 0x31
+export const  ETX = 0x03
--- src/protocolTest.js
+++ src/protocolTest.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { toHex } from './convertHex.js';
 import { calculateChk } from './calculateChecksum..js';
+import { SOH, DIGIT, ID0, ID1, ETX } from "./constants.js";
 
 class DigitProtocolTest extends Component {
     constructor(props) {
@@ -17,7 +18,7 @@ class DigitProtocolTest extends Component {
       //Change text string to ascii character and convert to hex.
       this.setState({myStringIsNowHex: e.target.value.charCodeAt(0).toString(16)});
       this.setState({buildDisplayCommand: e.target.value.charCodeAt(0).toString(16)});
-      this.setState({checksum:0x01+0x44+0x30+0x31}); 
+      //this.setState({checksum:0x01+0x44+0x30+0x31}); 
     }
     render() {
       return(
@@ -25,7 +26,7 @@ class DigitProtocolTest extends Component {
         <input type="text" onChange = {this.updateState}/>
           <h4>User input to hex: 0x{this.state.buildDisplayCommand === 'NaN' ? '00' : this.state.buildDisplayCommand}</h4>
           <p>
-          <h4>01 44 30 31 {this.state.buildDisplayCommand === 'NaN' ? '00' : this.state.buildDisplayCommand} {this.state.checksum.toString(16)} 03</h4>
+          <h4>{this.state.buildDisplayCommand === 'NaN' ? '00' : '0'+SOH+' '+DIGIT.toString(16)+' '+ID0.toString(16)+' '+ID1.toString(16)+' '+this.state.buildDisplayCommand} {this.state.checksum.toString(16)+ETX}</h4>
           </p>
         </div>
       );
