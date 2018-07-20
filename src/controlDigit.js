fbe69ab7797b9b08705ea8694ac9b07816b7098c
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-07-20 00:23:59 -0400

    Testing

--- src/controlDigit.js
+++ src/controlDigit.js
@@ -1,7 +1,6 @@
 import { calculateChk } from './calculateChecksum.js';
 import { SOH, DIGIT, ID0, ID1, ETX } from "./constants.js";
 
-
 export const displayCommand = s =>
 {
     var TEMP = parseInt(s.target.value.charCodeAt(0),10);
@@ -9,5 +8,5 @@ export const displayCommand = s =>
     var VAL = s.target.value.charCodeAt(0).toString(16);
     //console.log(SOH.toString(16)+DIGIT.toString(16)+ID0.toString(16)+ID1.toString(16)+VAL+CHK+ETX.toString(16));
     return SOH.toString(16)+DIGIT.toString(16)+ID0.toString(16)+ID1.toString(16)+VAL+CHK+ETX.toString(16);
-    
 }
+
