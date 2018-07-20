aa475321e37b76625271dad21656ecc85f8dd857
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-07-20 11:17:07 -0400

    Add list ports

--- src/listPorts.js
+++ src/listPorts.js
@@ -1,9 +1,8 @@
 
 var SerialPort = require('serialport');
-var SerialPort = serialport.SerialPort;
 
 //list serial ports
-serialport.list(function(err, ports){
+SerialPort.list(function(err, ports){
     ports.forEach(function(port){
         console.log(port.comName);
         console.log(port.pnpId);
