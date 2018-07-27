ea438bdc98c2646abef873aac9f80fa11dc3e7cd
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-07-27 15:01:58 -0400

    Test

--- src/testPort.js
+++ src/testPort.js
@@ -4,6 +4,16 @@ const  ID0 = 0x30
 const  ID1 = 0x31
 const  ETX = 0x03
 
+const calculateChk = s =>
+{
+    var TEMP = [];
+    //console.log(s);
+    TEMP = s.toString(16).split('',2)
+    //console.log(TEMP);
+    //console.log(toHex(TEMP[0]));
+    //console.log(toHex(TEMP[1]));
+    return toHex(TEMP[0])+toHex(TEMP[1]) ;
+}
 
 const displayCommand = s =>
 {
