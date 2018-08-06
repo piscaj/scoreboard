8292a4c8914843a0247fd6c383faad6e43253455
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-08-06 13:52:09 -0400

    Add REST server.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import TestDisplay from './api/scoreboard/testDisplayCommand'
 
+const express =  require('express');
+const app = express();
+
+App.use((req, res, next) => {
+  res.status(200).json({
+  message: 'It works!'
+  });
+});
+
 class App extends Component {
   render() {
     return (
