7c78ebc6d2a0e5abee57190aef5623f8e460cd89
Jason Piscatelli <JasonPiscatelli@Chriss-MBP.localdomain>
2018-08-07 14:02:55 -0400

    Updates

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import TestDisplay from './api/scoreboard/testDisplayCommand'
 
-const express =  require('express');
-const app = express();
-
-App.use((req, res, next) => {
-  res.status(200).json({
-  message: 'It works!'
-  });
-});
-
 class App extends Component {
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const http = require('http');
-const port = process.env.port || 3000;
-const server = http.createServer(App);
-server.listen(port);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
